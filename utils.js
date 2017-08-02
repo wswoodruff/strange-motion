edc6269ef9e3d11b436639879e020f7b01aad68c
William S. Woodruff <williamswoodruff@gmail.com>
2017-08-01 22:08:42 -0400

    Fix debounce util again

--- utils.js
+++ utils.js
@@ -60,25 +60,26 @@ module.exports = {
 
     debounce: (func, wait, immediate) => {
 
-    let timeout;
-    return (...args) => {
+        let timeout;
+        return (...args) => {
 
-        const self = this;
-        const later = () => {
+            const self = this;
+            const later = () => {
 
-            timeout = null;
-            if (!immediate) {
+                timeout = null;
+                if (!immediate) {
+                    func.apply(self, ...args);
+                }
+            };
+            const callNow = immediate && !timeout;
+
+            clearTimeout(timeout);
+
+            timeout = setTimeout(later, wait);
+
+            if (callNow) {
                 func.apply(self, ...args);
             }
-        };
-        const callNow = immediate && !timeout;
-
-        clearTimeout(timeout);
-
-        timeout = setTimeout(later, wait);
-
-        if (callNow) {
-            func.apply(self, ...args);
         }
-    };
+    }
 };
