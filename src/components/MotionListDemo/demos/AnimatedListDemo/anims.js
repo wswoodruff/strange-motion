6dc46db2e4df270d45fd6032b7d97a786a492f22
William S. Woodruff <williamswoodruff@gmail.com>
2017-08-07 19:49:54 -0400

    Tweak styles

--- src/components/MotionListDemo/demos/AnimatedListDemo/anims.js
+++ src/components/MotionListDemo/demos/AnimatedListDemo/anims.js
@@ -4,15 +4,15 @@ module.exports = {
     RowAnim: {
         startStyle: {
             height: 40,
-            opacity: 1
+            // opacity: 1
         },
         enterAnim: {
             height: 40,
-            opacity: 1
+            // opacity: 1
         },
         leaveAnim: {
             height: 0,
-            opacity: 0
+            // opacity: 0.3
         }
     }
 }
